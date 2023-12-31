139f3e4606fecd02689af0be85426ba7cde28043
Seungchan Lee <seunggs@gmail.com>
2023-12-30 18:53:10 -0800

    first commit

---
+++ target_iceberg/sinks.py
@@ -0,0 +1,103 @@
+"""Iceberg target sink class, which handles writing streams."""
+
+from __future__ import annotations
+from typing import Dict, List, Optional
+from singer_sdk import PluginBase
+from singer_sdk.sinks import BatchSink
+from pyspark.sql import SparkSession
+from configs import spark_conf
+
+
+class IcebergSink(BatchSink):
+    """Iceberg target sink class."""
+
+    max_size = 10000  # Max records to write in one batch
+
+    def __init__(
+        self,
+        target: PluginBase,
+        stream_name: str,
+        schema: Dict,
+        key_properties: Optional[List[str]],
+    ) -> None:
+        super().__init__(
+            target=target,
+            schema=schema,
+            stream_name=stream_name,
+            key_properties=key_properties,
+        )
+
+        # Accessing the properties from the target's config
+        self.aws_region = self.config.get("aws_region")
+        self.aws_access_key_id = self.config.get("aws_access_key_id")
+        self.aws_secret_access_key = self.config.get("aws_secret_access_key")
+        self.partition_by = self.config.get("partition_by", [])
+        self.table_name = self.config.get("table_name", "default")
+
+    # def start_batch(self, context: dict) -> None:
+    #     """Start a batch.
+
+    #     Developers may optionally add additional markers to the `context` dict,
+    #     which is unique to this batch.
+
+    #     Args:
+    #         context: Stream partition or context dictionary.
+    #     """
+    #     # Sample:
+    #     # ------
+    #     # batch_key = context["batch_id"]
+    #     # context["file_path"] = f"{batch_key}.csv"
+
+    # def process_record(self, record: dict, context: dict) -> None:
+    #     """Process the record.
+
+    #     Developers may optionally read or write additional markers within the
+    #     passed `context` dict from the current batch.
+
+    #     Args:
+    #         record: Individual record in the stream.
+    #         context: Stream partition or context dictionary.
+    #     """
+    #     # Sample:
+    #     # ------
+    #     # with open(context["file_path"], "a") as csvfile:
+    #     #     csvfile.write(record)
+
+    def process_batch(self, context: dict) -> None:
+        """Write out any prepped records and return once fully written.
+
+        Args:
+            context: Stream partition or context dictionary.
+
+        Required script for build:
+            `venv-pack -p ./.venv`
+        """
+        # Start Spark Session
+        spark = SparkSession.builder.config(conf=spark_conf).getOrCreate()
+        print("Spark Running")
+
+        # Create a Spark dataframe
+        headers = self.schema["properties"].keys()
+        df = spark.createDataframe(context["records"], headers)
+
+        # Create a temp view of the dataframe so it can be selected via SQL
+        df.createOrReplaceTempView("records_temp_view")
+
+        # Create an Iceberg table
+        partition_clause = (
+            ""
+            if not self.partition_by
+            else f"PARTITIONED BY ({', '.join(self.partition_by)})"
+        )
+
+        spark.sql(
+            f"CREATE TABLE IF NOT EXISTS nessie.{self.table_name} USING iceberg {partition_clause}"
+        ).show()
+
+        # Write the dataframe to the Iceberg table
+        unique_col = self.key_properties[0]
+        spark.sql(
+            f"""MERGE INTO nessie.{self.table_name} t USING (SELECT * FROM records_temp_view) u ON t.{unique_col} = u.{unique_col}
+                WHEN MATCHED THEN UPDATE SET *
+                WHEN NOT MATCHED THEN INSERT *"""
+        ).show()
---
+++ target_iceberg/target.py
@@ -0,0 +1,34 @@
+"""Iceberg target class."""
+
+from __future__ import annotations
+
+from singer_sdk import typing as th
+from singer_sdk.target_base import Target
+
+from target_iceberg.sinks import (
+    IcebergSink,
+)
+
+
+class TargetIceberg(Target):
+    name = "target-iceberg"
+
+    config_jsonschema = th.PropertiesList(
+        th.Property("add_record_metadata", th.BooleanType, default=False),
+        th.Property("aws_region", th.StringType, required=True),
+        th.Property("aws_access_key_id", th.StringType),
+        th.Property("aws_secret_access_key", th.StringType),
+        th.Property("aws_session_token", th.StringType),
+        th.Property("aws_profile", th.StringType),
+        th.Property("s3_bucket", th.StringType, required=True),
+        th.Property("s3_key_prefix", th.StringType),
+        th.Property("table_name", th.StringType),
+        th.Property("partition_by", th.StringType),
+
+    ).to_dict()
+
+    default_sink_class = IcebergSink
+
+
+if __name__ == "__main__":
+    TargetIceberg.cli()
