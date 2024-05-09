2256ca041ce54f97b816cc5db24c0cf9edb0ec3c
Seungchan Lee <seunggs@gmail.com>
2024-05-08 17:24:56 -0700

    add logging

--- target_iceberg/sinks.py
+++ target_iceberg/sinks.py
@@ -51,8 +51,11 @@ class IcebergSink(BatchSink):
         # IMPORTANT: Make sure pyiceberg catalog env variables are set in the host machine - i.e. PYICEBERG_CATALOG__DEFAULT__URI, etc
         #   - For more details, see: https://py.iceberg.apache.org/configuration/)
         region = fs.resolve_s3_region(self.config.get("s3_bucket"))
-        print('AWS Region: ', region)
+        self.logger.info(f'AWS Region: {region}')
+        
         catalog_name = self.config.get("iceberg_catalog_name")
+        self.logger.info(f"Catalog name: {catalog_name}")
+        
         catalog = load_catalog(
             catalog_name,
             **{
