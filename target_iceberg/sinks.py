14b4384b6b2e408f3144a43f8dbcf9a602483711
Seungchan Lee <seunggs@gmail.com>
2024-05-10 13:02:37 -0700

    fix the logger

--- target_iceberg/sinks.py
+++ target_iceberg/sinks.py
@@ -92,7 +92,7 @@ class IcebergSink(BatchSink):
             # TODO: Handle schema evolution - compare existing table schema with singer schema (converted to pyiceberg schema)
         except NoSuchTableError as e:
             # Table doesn't exist, so create it
-            table_schema = singer_to_pyiceberg_schema(self, singer_schema)
+            table_schema = singer_to_pyiceberg_schema(singer_schema, self.logger)
             table = catalog.create_table(table_id, schema=table_schema)
             self.logger.info(f"Table '{table_id}' created")
 
