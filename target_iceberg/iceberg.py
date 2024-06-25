aa2706db968ac7e5aa906973a86a559877fd7a73
taeefnajib <taeefnajib@gmail.com>
2024-06-26 05:48:49 +0600

    add if properties is none return fields

--- target_iceberg/iceberg.py
+++ target_iceberg/iceberg.py
@@ -62,8 +62,10 @@ def singer_to_pyarrow_schema_without_field_ids(self, singer_schema: dict) -> Pya
         Returns schema for an object.
         """
         fields = []
-        self.logger.info(f"*********PROPERTIES: {properties}*********")
-        self.logger.info(f"*********LEVEL: {level}*********")
+        # self.logger.info(f"*********PROPERTIES: {properties}*********")
+        # self.logger.info(f"*********LEVEL: {level}*********")
+        if properties is None:
+            return fields
         for key, val in properties.items():
             if "type" in val.keys():
                 type = val["type"]
