d5fc1ac6a48e42fcf1e96a8b3f895fb5fc83a4dc
taeefnajib <taeefnajib@gmail.com>
2024-07-04 13:27:19 +0600

    object in type handled

--- target_iceberg/iceberg.py
+++ target_iceberg/iceberg.py
@@ -72,7 +72,19 @@ def singer_to_pyarrow_schema_without_field_ids(self, singer_schema: dict) -> Pya
                 self.logger.warning("type information not given")
                 type = ["string", "null"]
 
-            if "integer" in type:
+            if "object" in type:
+                nullable = "null" in type
+                prop = val.get("properties")
+                inner_fields = get_pyarrow_schema_from_object(properties=prop, level=level + 1)
+                if not inner_fields:
+                    self.logger.warn(
+                        f"""key: {key} has no fields defined, this may cause
+                            saving parquet failure as parquet doesn't support
+                            empty/null complex types [array, structs] """
+                    )
+                fields.append(pa.field(key, pa.struct(inner_fields), nullable=nullable))
+
+            elif "integer" in type:
                 nullable = "null" in type
                 fields.append(pa.field(key, pa.int64(), nullable=nullable))
             elif "number" in type:
@@ -113,18 +125,9 @@ def singer_to_pyarrow_schema_without_field_ids(self, singer_schema: dict) -> Pya
                             exact item types for the list, if not null."""
                     )
                     fields.append(pa.field(key, pa.list_(pa.null()), nullable=nullable))
-            elif "object" in type:
+            else:
                 nullable = "null" in type
-                prop = val.get("properties")
-                inner_fields = get_pyarrow_schema_from_object(properties=prop, level=level + 1)
-                if not inner_fields:
-                    self.logger.warn(
-                        f"""key: {key} has no fields defined, this may cause
-                            saving parquet failure as parquet doesn't support
-                            empty/null complex types [array, structs] """
-                    )
-                fields.append(pa.field(key, pa.struct(inner_fields), nullable=nullable))
-
+                fields.append(pa.field(key, pa.null(), nullable=nullable))
         return fields
 
     properties = singer_schema["properties"]
