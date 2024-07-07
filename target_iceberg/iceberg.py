30aeef07c49d699d20797c7903f8ce12d51aae5e
taeefnajib <taeefnajib@gmail.com>
2024-07-07 22:20:32 +0600

    add unknown to null dict

--- target_iceberg/iceberg.py
+++ target_iceberg/iceberg.py
@@ -68,7 +68,7 @@ def singer_to_pyarrow_schema_without_field_ids(self, singer_schema: dict) -> Pya
         if not properties:
             self.logger.warning(f"**********No properties found for the object at level: {level}**********")
             #{'unknown': {'type': ['string', 'null']}}
-            #fields.append(pa.field('unknown', pa.string(), nullable=True))
+            fields.append(pa.field('unknown', pa.string(), nullable=True))
             #fields.append(pa.field(key, pa.struct(inner_fields), nullable=True))
             return fields
 
