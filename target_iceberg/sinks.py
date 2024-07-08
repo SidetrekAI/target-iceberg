7cc07e98e1c16b10995a95f71e9d36c3f985792e
taeefnajib <taeefnajib@gmail.com>
2024-07-08 08:28:33 +0600

    add filter

--- target_iceberg/sinks.py
+++ target_iceberg/sinks.py
@@ -88,7 +88,10 @@ class IcebergSink(BatchSink):
         for record in records:
             for key, value in record.items():
                 if isinstance(value, dict):
-                    record[key] = json.dumps(value)
+                    if not value:  # Check if the dictionary is empty
+                        record[key] = None  # Replace empty dictionary with None or an appropriate sentinel value
+                    else:
+                        record[key] = json.dumps(value)  # Serialize non-empty dictionaries
                     self.logger.info(f"Serialized {key}: {record[key]}")
 
         self.logger.info(f"Serialized Records: {records}")
